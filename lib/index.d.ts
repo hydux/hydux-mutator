13d5fed9cf72523d8fe5681225346ae9370b89ad
zaaack <zaaack@qq.com>
2018-01-24 19:47:30 +0800

    0.1.9

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -8,9 +8,7 @@ export declare function setCacheSize(maxSize?: number): void;
  * @param accessor A lambda function to get the key path, support dot, [''], [""], [1], **do not** support dynamic variable, function call, e.g.
  * @param ctx Dynamic key map.
  */
-export declare function getIn<T, V>(record: T, accessor: ((obj: T) => V) | string[], ctx?: {
-    [key: string]: string;
-}): V;
+export declare function getIn<T, V>(record: T, accessor: ((obj: T) => V) | string[], ctx?: (string | number)[]): V;
 /**
  * Set a deep child
  *
@@ -19,9 +17,7 @@ export declare function getIn<T, V>(record: T, accessor: ((obj: T) => V) | strin
  * @param value The new value to set, if it is ignored it will be set to undefined.
  * @param ctx Dynamic key map.
  */
-export declare function setIn<T, V>(record: T, accessor: ((obj: T) => V) | string[], value?: V, ctx?: {
-    [key: string]: string;
-}): T;
+export declare function setIn<T, V>(record: T, accessor: ((obj: T) => V) | string[], value?: V, ctx?: (string | number)[]): T;
 /**
  * Unset a deep child
  *
@@ -29,9 +25,7 @@ export declare function setIn<T, V>(record: T, accessor: ((obj: T) => V) | strin
  * @param accessor A lambda function to get the key path, support dot, [''], [""], [1], **do not** support dynamic variable, function call, e.g.
  * @param ctx Dynamic key map.
  */
-export declare function unsetIn<T, V>(record: T, accessor: ((obj: T) => V | undefined) | string[], ctx?: {
-    [key: string]: string;
-}): T;
+export declare function unsetIn<T, V>(record: T, accessor: ((obj: T) => V | undefined) | string[], ctx?: (string | number)[]): T;
 /**
  * Update a deep child by old value
  * @param record The object to update, it can be a plain object or a class instance
@@ -39,18 +33,10 @@ export declare function unsetIn<T, V>(record: T, accessor: ((obj: T) => V | unde
  * @param updator A update function that take the old value and return the new value.
  * @param ctx Dynamic key map.
  */
-export declare function updateIn<T, V, A>(record: T, accessor: ((obj: T) => V) | string[], updator?: (v: V) => V, ctx?: {
-    [key: string]: string;
-}): T;
+export declare function updateIn<T, V, A>(record: T, accessor: ((obj: T) => V) | string[], updator?: (v: V) => V, ctx?: (string | number)[]): T;
 declare const _default: {
-    setIn: <T, V>(record: T, accessor: string[] | ((obj: T) => V), value?: V | undefined, ctx?: {
-        [key: string]: string;
-    } | undefined) => T;
-    unsetIn: <T, V>(record: T, accessor: string[] | ((obj: T) => V | undefined), ctx?: {
-        [key: string]: string;
-    } | undefined) => T;
-    updateIn: <T, V, A>(record: T, accessor: string[] | ((obj: T) => V), updator?: ((v: V) => V) | undefined, ctx?: {
-        [key: string]: string;
-    } | undefined) => T;
+    setIn: <T, V>(record: T, accessor: string[] | ((obj: T) => V), value?: V | undefined, ctx?: (string | number)[] | undefined) => T;
+    unsetIn: <T, V>(record: T, accessor: string[] | ((obj: T) => V | undefined), ctx?: (string | number)[] | undefined) => T;
+    updateIn: <T, V, A>(record: T, accessor: string[] | ((obj: T) => V), updator?: ((v: V) => V) | undefined, ctx?: (string | number)[] | undefined) => T;
 };
 export default _default;
